53ae6abf4e36dd3db538689e2400f0490e8e8847
Tourtle21 <kardlargin@gmail.com>
2020-04-29 22:32:27 -0600

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './pages/Home'
+import Classes from './pages/Classes'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Teacher Tracker
-        </h1>
-
-      </header>
-    </div>
+    <Router>
+    <div>Header</div>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/classes" component={Classes} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Classes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Classes() {
+  return (
+    <h1>Classes Page</h1>
+  );
+}
+
+export default Classes;
