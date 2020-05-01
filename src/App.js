937fbd1545e06fa1b9983d79098bfcd6074e9f64
Tourtle21 <kardlargin@gmail.com>
2020-04-30 22:51:14 -0600

    add firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Home from "./pages/Home";
 import Classes from "./pages/Classes";
 import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCIxQyV0VSvnQY6Hsrg4SjYjcn0hWiF4B0",
+    authDomain: "teacher-tracker-1b566.firebaseapp.com",
+    databaseURL: "https://teacher-tracker-1b566.firebaseio.com",
+    projectId: "teacher-tracker-1b566",
+    storageBucket: "teacher-tracker-1b566.appspot.com",
+    messagingSenderId: "477834957440",
+    appId: "1:477834957440:web:2d6cb0e177b23c63e50c65",
+    measurementId: "G-NT12D2KCVM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 function App() {
   return (
     <Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import * as firebase from 'firebase';
 
-function Home() {
-  return (
-    <h1>Home Page</h1>
-  );
+class Home extends React.Component {
+
+  state = {
+    speed: 10
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      })
+    })
+  }
+
+  render() {
+    return (
+      <h1>{this.state.speed}</h1>
+    );
+  }
 }
 
 export default Home;
